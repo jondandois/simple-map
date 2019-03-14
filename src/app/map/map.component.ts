bd801627bbe3cc7817279d2c95e41e0a8b4a6e8b
jdandois <jon.dandois@gmail.com>
2019-03-13 23:07:52 -0400

    adding in grid for map layout

---
+++ src/app/map/map.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+// declare leaflet var
+declare let L;
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
+})
+
+export class MapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+
+    const map = L.map('map').setView([39.288299, -76.613486], 11);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+  }
+
+}
